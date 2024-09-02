8b3c9b73e2724f5244f9f89832e90878e8741706
Mustafa <mustafa.gamal.elsayed@gmail.com>
2024-09-02 20:32:45 +0300

    --fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ export default function Header({ setIsMenuOpen }) {
   return (
     <>
       {/* Site header */}
-      <header className="site-header padding-left-container">
+      <header className="site-header ">
         {/* Mobile header */}
         <div id="atbs-mobile-header" className="mobile-header ">
           <div className="mobile-header__inner mobile-header__inner--flex">
